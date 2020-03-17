dfee3c147414f017ffd05f633af33c6219237f7d
Fabio Oquendo <fabio.oquendo@castor.com.co>
2020-03-16 21:54:52 -0500

    init angular project

---
+++ src/app/home/components/banner/banner.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+
+  images: string[] = [
+    'assets/images/Castor-PWA.png',
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
